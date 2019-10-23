8dc8ce729c4e3fc911ba3556e7a1548d771e8f5a
Jitendar Mandali <jitendar7@gmail.com>
2019-10-23 17:52:38 +0400

    updated live data viewmodel

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -23,7 +23,9 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import com.example.android.viewmodel.GameViewModel
 import com.example.androidexperiments.R
 import com.example.androidexperiments.databinding.FragmentGameBinding
 
@@ -32,38 +34,11 @@ class GameFragment : Fragment() {
         val text: String,
         val answers: List<String>)
 
-    // The first answer is the correct one.  We randomize the answers before showing the text.
-    // All questions must have four answers.  We'd want these to contain references to string
-    // resources so we could internationalize. (Or better yet, don't define the questions in code...)
-    private val questions: MutableList<Question> = mutableListOf(
-        Question(text = "What is Android Jetpack?",
-            answers = listOf("All of these", "Tools", "Documentation", "Libraries")),
-        Question(text = "What is the base class for layouts?",
-            answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
-        Question(text = "What layout do you use for complex screens?",
-            answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
-        Question(text = "What do you use to push structured data into a layout?",
-            answers = listOf("Data binding", "Data pushing", "Set text", "An OnClick method")),
-        Question(text = "What method do you use to inflate layouts in fragments?",
-            answers = listOf("onCreateView()", "onActivityCreated()", "onCreateLayout()", "onInflateLayout()")),
-        Question(text = "What's the build system for Android?",
-            answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")),
-        Question(text = "Which class do you use to create a vector drawable?",
-            answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
-        Question(text = "Which one of these is an Android navigation component?",
-            answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
-        Question(text = "Which XML element lets you register an activity with the launcher activity?",
-            answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
-        Question(text = "What do you use to mark a layout for data binding?",
-            answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
-    )
-
-
-
     lateinit var currentQuestion: Question
     lateinit var answers: MutableList<String>
     private var questionIndex = 0
-    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+    private var numQuestions: Int = 0
+    private lateinit var viewModel: GameViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -72,11 +47,17 @@ class GameFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentGameBinding>(
             inflater, R.layout.fragment_game, container, false)
 
+        //creating a viewmodel
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+
+
         // Shuffles the questions and sets the question index to the first question.
         randomizeQuestions()
 
         // Bind this fragment class to the layout
         binding.game = this
+
+        numQuestions = Math.min((viewModel.questions.size + 1) / 2, 3)
 
         // Set the onClickListener for the submitButton
         binding.submitButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
@@ -96,7 +77,7 @@ class GameFragment : Fragment() {
                     questionIndex++
                     // Advance to the next question
                     if (questionIndex < numQuestions) {
-                        currentQuestion = questions[questionIndex]
+                        currentQuestion = viewModel.questions[questionIndex]
                         setQuestion()
                         binding.invalidateAll()
                     } else {
@@ -110,12 +91,13 @@ class GameFragment : Fragment() {
                 }
             }
         }
+
         return binding.root
     }
 
     // randomize the questions and set the first question
     private fun randomizeQuestions() {
-        questions.shuffle()
+        viewModel.questions.shuffle()
         questionIndex = 0
         setQuestion()
     }
@@ -123,7 +105,7 @@ class GameFragment : Fragment() {
     // Sets the question and randomizes the answers.  This only changes the data, not the UI.
     // Calling invalidateAll on the FragmentGameBinding updates the data.
     private fun setQuestion() {
-        currentQuestion = questions[questionIndex]
+        currentQuestion = viewModel.questions[questionIndex]
         // randomize the answers into a copy of the array
         answers = currentQuestion.answers.toMutableList()
         // and shuffle them
--- app/src/main/java/com/example/android/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/example/android/viewmodel/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.android.navigation.GameFragment
 import timber.log.Timber
 
 
@@ -11,6 +12,62 @@ import timber.log.Timber
 // retained until the fragment is detached
 
 class GameViewModel : ViewModel() {
+
+    // The first answer is the correct one.  We randomize the answers before showing the text.
+    // All questions must have four answers.  We'd want these to contain references to string
+    // resources so we could internationalize. (Or better yet, don't define the questions in code...)
+    val questions: MutableList<GameFragment.Question> = mutableListOf(
+        GameFragment.Question(
+            text = "What is Android Jetpack?",
+            answers = listOf("All of these", "Tools", "Documentation", "Libraries")
+        ),
+        GameFragment.Question(
+            text = "What is the base class for layouts?",
+            answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")
+        ),
+        GameFragment.Question(
+            text = "What layout do you use for complex screens?",
+            answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")
+        ),
+        GameFragment.Question(
+            text = "What do you use to push structured data into a layout?",
+            answers = listOf("Data binding", "Data pushing", "Set text", "An OnClick method")
+        ),
+        GameFragment.Question(
+            text = "What method do you use to inflate layouts in fragments?",
+            answers = listOf(
+                "onCreateView()",
+                "onActivityCreated()",
+                "onCreateLayout()",
+                "onInflateLayout()"
+            )
+        ),
+        GameFragment.Question(
+            text = "What's the build system for Android?",
+            answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")
+        ),
+        GameFragment.Question(
+            text = "Which class do you use to create a vector drawable?",
+            answers = listOf(
+                "VectorDrawable",
+                "AndroidVectorDrawable",
+                "DrawableVector",
+                "AndroidVector"
+            )
+        ),
+        GameFragment.Question(
+            text = "Which one of these is an Android navigation component?",
+            answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")
+        ),
+        GameFragment.Question(
+            text = "Which XML element lets you register an activity with the launcher activity?",
+            answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")
+        ),
+        GameFragment.Question(
+            text = "What do you use to mark a layout for data binding?",
+            answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>")
+        )
+    )
 
     //ViewModel - onCleared() callback is called to clean up the resources
     override fun onCleared() {
